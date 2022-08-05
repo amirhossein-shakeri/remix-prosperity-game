15faba2bc913156125c2b427947c6f5ab7928f27
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2022-08-05 05:47:42 -0400

    initial commit

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,25 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import type { Post } from "~/models/post.server";
+import { getPost } from "~/models/post.server";
+
+type LoaderData = {
+  post: Post;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, "params.slug is required");
+  const post = await getPost(params.slug);
+  invariant(post, `post not found ${params.slug}`);
+  return json<LoaderData>({ post });
+};
+
+export default function PostSlug() {
+  const { post } = useLoaderData() as unknown as LoaderData;
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+    </main>
+  );
+}
